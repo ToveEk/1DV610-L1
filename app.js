7d656db08f0ed1eb5c177ae5fce73537cd50490f
Tove Ek <te222uu@student.lnu.se>
2025-09-03 12:45:31 +0200

    Add readline-sync for user input and update app.js to greet user

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import readline from "readline-sync"
+
+console.log("Hello friend! What's your name?")
+
+const name = readline.question("Enter your name: ")
+
+console.log(`Nice to meet you, ${name}!`)
