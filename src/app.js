39f1b7bd07e805c5a17fe06f2d767b9259620a1a
Tove Ek <te222uu@student.lnu.se>
2025-09-03 14:45:58 +0200

    Simplify user prompts in app.js by removing default input messages

--- src/app.js
+++ src/app.js
@@ -8,11 +8,10 @@ import { gameGenerator } from "./games.js"
 
 console.log("Hello friend! What's your name?")
 
-const name = readline.question("Enter your name: ")
+const name = readline.question()
 
-console.log(`Nice to meet you, ${name}!`)
-console.log("What is your favorite video game?")
+console.log(`Nice to meet you, ${name}!` + "\n" + "What is your favorite video game?")
 
-const favoriteGame = readline.question("Enter your favorite video game: ")
+const favoriteGame = readline.question()
 
 console.log(gameGenerator(favoriteGame))
